cca332cf4d5ff86842d8ebf606e6b5fb296e39bf
mitali gautam <gautammitali97@gmail.com>
2026-08-10 15:38:09 +0530

    TypedDict from typing implemented for the structured output

---
+++ Langchain_structured_output/TypedDict_demo.py
@@ -0,0 +1,9 @@
+from typing import TypedDict
+
+class Person (TypedDict):
+    name : str
+    age : int
+    Year : int 
+
+one_Person : Person  ={'name':'shikha' ,'age':'19','Year':'3'}
+print(one_Person)

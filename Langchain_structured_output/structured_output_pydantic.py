fe3ad249b802892a22c4a7f307e1795c755fba02
mitali gautam <gautammitali97@gmail.com>
2026-08-11 19:33:27 +0530

    Structured output with pydantic for validation

---
+++ Langchain_structured_output/structured_output_pydantic.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from typing import Optional, Literal
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V4-Flash-0731",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+# schema
+class Review(BaseModel):
+    key_themes: list[str] = Field(description="Write down all the key themes discussed in the review in a list")
+    summary: str = Field(description="A brief summary of the review")
+    sentiment: Literal["pos", "neg"] = Field(description="Return sentiment of the review either negative, positive or neutral")
+    pros: Optional[list[str]] = Field(default=None, description="Write down all the pros inside a list")
+    cons: Optional[list[str]] = Field(default=None, description="Write down all the cons inside a list")
+    name: Optional[str] = Field(default=None, description="Write the name of the reviewer")
+
+parser = PydanticOutputParser(pydantic_object=Review)
+
+prompt = PromptTemplate(
+    template="Answer the user query based on the given text.\n{format_instructions}\n{review_text}\n",
+    input_variables=["review_text"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
+)
+
+review_text = """I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it's an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I'm gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung's One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+
+Review by Mitali Gautam
+"""
+
+chain = prompt | model | parser
+
+result = chain.invoke({"review_text": review_text})
+
+print(result.sentiment)

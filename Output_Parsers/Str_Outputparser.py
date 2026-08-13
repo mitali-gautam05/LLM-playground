14d9371202ec05ac008ca7fee20559d49dc708a0
mitali gautam <gautammitali97@gmail.com>
2026-08-13 20:36:26 +0530

    String output parser is basic

---
+++ Output_Parsers/Str_Outputparser.py
@@ -0,0 +1,30 @@
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id = "meta-llama/Llama-3.1-8B-Instruct",
+    task = "text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+# prompt1 ->detailed report
+template1 = PromptTemplate (
+    template = 'Write detailed report,{topic}',
+    input_variables = ['topic'])
+
+# prompt2
+template2 = PromptTemplate (
+    template = 'Write 5 line summary on the following text . /n{text}',
+    input_variables = ['text'])
+
+prompt1 = template1.invoke({'topic':'blackhole'})
+result1 = model.invoke(prompt1)
+
+prompt2 = template2.invoke({'text':result1.content})
+result2 = model.invoke(prompt2)
+
+print(result2.content)

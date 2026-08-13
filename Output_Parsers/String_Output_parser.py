eb41fe99ae134aef70848ae64ffe67c82414feb5
mitali gautam <gautammitali97@gmail.com>
2026-08-13 20:33:43 +0530

    String output parser

---
+++ Output_Parsers/String_Output_parser.py
@@ -0,0 +1,30 @@
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id = "meta-llama/Llama-3.1-8B-Instruct",
+    task = "text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+# prompt1 ->detailed report
+template1 = PromptTemplate (
+    template = 'Write detailed report,{topic}',
+    input_variables = ['topic'])
+
+# prompt2
+template2 = PromptTemplate (
+    template = 'Write 5 line summary on the following text . /n{text}',
+    input_variables = ['text'])
+
+parser = StrOutputParser()
+
+chain = template1 | model | parser | template2 | model | parser
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
